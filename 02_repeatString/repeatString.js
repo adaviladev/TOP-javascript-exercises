de1881a5b09caf2b46920cb30681bcfd4aaaef29
adaviladev <alvarodaviladev@gmail.com>
2024-08-05 00:04:32 -0400

    complete excercise 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let getString = "";  
+    let i = 0; 
+    if (num < 0) {
+        return 'ERROR'
+    }
+    while(i<num) {
 
+        getString += string;
+        i++;
+    }
+    return getString;
 };
+
+repeatString('hi', 1)
 
 // Do not edit below this line
 module.exports = repeatString;
