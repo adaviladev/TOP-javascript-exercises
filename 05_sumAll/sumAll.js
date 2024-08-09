b6939ea2aad1f7eb739ddb27840362aa9b201a73
adaviladev <alvarodaviladev@gmail.com>
2024-08-08 21:09:41 -0400

    done exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    const isValidNumber = (num) => Number.isInteger(num) && !isNaN(num) && num >= 0;
+
+    if (!isValidNumber(num1) || !isValidNumber(num2)) {
+        return "ERROR"
+    }
+    
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+
+    // if (
+    //     sum <= 0 ||
+    //     !Number.isInteger(num1) ||
+    //     !Number.isInteger(num2) ||
+    //     isNaN(num1) ||
+    //     isNaN(num2)) {
+    //     return "ERROR"
+    // }
+
+    return sum
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let celcius = (number - 32) * (5/9);
+  let rounded = Math.round(celcius * 10) / 10;
+  return parseFloat(rounded);
 };
 
-const convertToFahrenheit = function() {
+  
+const convertToFahrenheit = function(number) {
+  let fahrenheit = (number * (9/5)) + 32
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return parseFloat(rounded);
 };
 
 // Do not edit below this line
