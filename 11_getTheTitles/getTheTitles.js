0866deefbae6d632b1a845b1cd6cb6fe89ffa3a4
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-10-09 13:08:24 -0300

    done book exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let bookTitles = [];
+    bookTitles = arr.map((arr) => arr.title)
+    return bookTitles;
 };
 
 // Do not edit below this line
