a1911a0e8a849bada8c911e7415d09252f395cd9
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-10-09 01:40:27 -0300

    done fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const num = parseInt(n);
 
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  if (num === 0) {
+    return 0;
+  }
+
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  let a = 1;
+  let b = 1;
+
+  for (let i = 3; i <= num; i++) {
+    let newFibonnaci = a + b;
+    a = b;
+    b = newFibonnaci;
+  }
+  return b;
 };
 
 // Do not edit below this line
