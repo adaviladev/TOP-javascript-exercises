e7de212ad0276ddbd813c6e9fe302f29818414d4
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-10-09 00:43:03 -0300

    done palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Convertir a minúsculas y limpiar la cadena de caracteres no válidos
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, ""); // Reemplazar todo lo que no sea letras o números con una cadena vacía
 
+  // Comprobar si la cadena limpiada es un palíndromo
+  const isPalindrome = cleanedStr === cleanedStr.split("").reverse().join("");
+  return isPalindrome; // Devuelve true o false
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -4,25 +4,36 @@ describe('palindromes', () => {
   test('works with single words', () => {
     expect(palindromes('racecar')).toBe(true);
   });
-  test.skip('works with punctuation ', () => {
+  test('works with punctuation ', () => {
     expect(palindromes('racecar!')).toBe(true);
   });
-  test.skip('works with upper-case letters ', () => {
+  test('works with upper-case letters ', () => {
     expect(palindromes('Racecar!')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
     expect(palindromes('A car, a man, a maraca.')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
     expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
   });
-  test.skip('doesn\'t just always return true', () => {
-    expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
-  });
-  test.skip('works with numbers in a string', () => {
+const palindromes = function (str) {
+  // Convertir a minúsculas y limpiar la cadena de caracteres no válidos
+  const cleanedStr = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, ''); // Reemplazar todo lo que no sea letras o números con una cadena vacía
+
+  // Comprobar si la cadena limpiada es un palíndromo
+  const isPalindrome = cleanedStr === cleanedStr.split('').reverse().join('');
+  return isPalindrome; // Devuelve true o false
+};
+
+// Do not edit below this line
+module.exports = palindromes;
+
+  test('works with numbers in a string', () => {
     expect(palindromes('rac3e3car')).toBe(true);
   });
-  test.skip('works with unevenly spaced numbers in a string', () => {
+  test('works with unevenly spaced numbers in a string', () => {
     expect(palindromes('r3ace3car')).toBe(false);
   });
 });
